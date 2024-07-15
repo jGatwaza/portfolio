2419c127a0753d11768725cc6870353dd221895b
Jean Yves Gatwaza <104692473+jGatwaza@users.noreply.github.com>
2024-07-15 11:45:03 -0400

    Responsiveness added to all components

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,46 @@
-import { ContactShadows, Environment, OrbitControls, Sky } from "@react-three/drei";
+import React, { useEffect, useState } from "react";
+import {
+  ContactShadows,
+  Environment,
+  OrbitControls,
+  Sky,
+} from "@react-three/drei";
 import { Avatar } from "./Avatar";
 
 export const Experience = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 1400);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1400);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
       {/* <OrbitControls /> */}
       <Sky />
       <Environment preset="sunset" />
-      <group position-y={-1} position-x={1.5}>
-        <ContactShadows scale={10} opacity={0.42} position-x={-1.5} resolution={256} color="#000000" far={10} blur={1}/>
+      <group position-y={-1} position-x={isMobile ? 0 : 1.5}>
+        <ContactShadows
+          scale={10}
+          opacity={0.42}
+          position-x={isMobile ? 0 : -1.5}
+          resolution={256}
+          color="#000000"
+          far={10}
+          blur={1}
+        />
         <Avatar />
-        <mesh receiveShadow scale={6.5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
+        <mesh
+          receiveShadow
+          scale={6.5}
+          rotation-x={-Math.PI * 0.5}
+          position-y={-0.001}
+        >
           <planeGeometry />
           <meshStandardMaterial color="white" />
         </mesh>
